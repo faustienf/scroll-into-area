af9c7b3b37f2c566f597c8eba11a01e0a1cd3dcc
faustienf <faustienf@protonmail.com>
2022-12-29 00:59:42 +0300

    add scroll-into-area

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,41 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import { scrollIntoArea } from "./scroll-into-area";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const rows = Array(5)
+  .fill(
+    `
+  <li></li> 
+  <li></li> 
+  <li></li>
+  <li></li>
+  <li></li>
+`.trim()
+  )
+  .join("\n");
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <button>↖️</button>
+  <button>⬆️</button>
+  <button>↗️</button>
+
+  <button>⬅️</button>
+  <ul class="container">
+    ${rows} 
+  </ul>
+  <button>➡️</button>
+
+  <button>↙️</button>
+  <button>⬇️</button>
+  <button>↘️</button>
+`;
+
+const container = document.querySelector("#app > ul")!;
+const target = document.querySelector("#app > ul > li:nth-child(13)")!;
+
+scrollIntoArea({
+  container,
+  target,
+  y: "center",
+  x: "center",
+});
---
+++ src/scroll-into-area.ts
@@ -0,0 +1,108 @@
+type Position = "start" | "end" | "center";
+
+type Options = {
+  container: Element;
+  target: Element;
+  x?: Position;
+  y?: Position;
+};
+
+const getStartScroll = (
+  containerScroll: number,
+  containerOffset: number,
+  targetOffset: number
+): number => containerScroll - containerOffset + targetOffset;
+
+const getEndScroll = (
+  containerScroll: number,
+  containerOffset: number,
+  containerSize: number,
+  targetOffset: number,
+  targetSize: number
+) => {
+  const offset = targetOffset - containerOffset - containerSize;
+  return containerScroll + offset + targetSize;
+};
+
+const getCenterScroll = (
+  containerScroll: number,
+  containerOffset: number,
+  containerSize: number,
+  targetOffset: number,
+  targetSize: number
+) => {
+  const offset = targetOffset - containerOffset - containerSize / 2;
+  return containerScroll + offset + targetSize / 2;
+};
+
+export const scrollIntoArea = ({ container, target, x, y }: Options) => {
+  const containerRect = container.getBoundingClientRect();
+  const targetRect = target.getBoundingClientRect();
+
+  let top: number | undefined;
+  let left: number | undefined;
+
+  switch (x) {
+    case "start": {
+      left = getStartScroll(
+        container.scrollLeft,
+        containerRect.left,
+        targetRect.left
+      );
+      break;
+    }
+    case "end": {
+      left = getEndScroll(
+        container.scrollLeft,
+        containerRect.left,
+        containerRect.width,
+        targetRect.left,
+        targetRect.width
+      );
+      break;
+    }
+    case "center": {
+      left = getCenterScroll(
+        container.scrollLeft,
+        containerRect.left,
+        containerRect.width,
+        targetRect.left,
+        targetRect.width
+      );
+      break;
+    }
+  }
+
+  switch (y) {
+    case "start": {
+      top = getStartScroll(
+        container.scrollTop,
+        containerRect.top,
+        targetRect.top
+      );
+      break;
+    }
+    case "end": {
+      top = getEndScroll(
+        container.scrollTop,
+        containerRect.top,
+        containerRect.height,
+        targetRect.top,
+        targetRect.height
+      );
+      break;
+    }
+    case "center": {
+      top = getCenterScroll(
+        container.scrollTop,
+        containerRect.top,
+        containerRect.height,
+        targetRect.top,
+        targetRect.height
+      );
+      break;
+    }
+  }
+
+  container.scrollTo({ top, left });
+};
